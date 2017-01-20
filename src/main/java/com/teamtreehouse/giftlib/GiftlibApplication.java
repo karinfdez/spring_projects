10ca86782b2eaf6f8d6cf1803bc07d4bdf24a501
Karin Fernandez <karinfdez@yahoo.com>
2017-01-20 01:10:17 -0500

    Added first Spring project

---
+++ src/main/java/com/teamtreehouse/giftlib/GiftlibApplication.java
@@ -0,0 +1,12 @@
+package com.teamtreehouse.giftlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftlibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftlibApplication.class, args);
+	}
+}
