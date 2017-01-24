0c260e5b10769b11375af08b96f181c9f25c0dc4
Karin Fernandez <karinfdez@yahoo.com>
2017-01-23 22:38:39 -0500

    added variable expression to the a view

--- src/main/java/com/teamtreehouse/giftlib/controller/GiftController.java
+++ src/main/java/com/teamtreehouse/giftlib/controller/GiftController.java
@@ -1,13 +1,26 @@
 package com.teamtreehouse.giftlib.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.teamtreehouse.giftlib.model.Gif;
 
 
 @Controller
 public class GiftController {
+
 	@RequestMapping("/")
 	public String listGifs(){
 		return "home";
+	}
+	
+	@RequestMapping("/gif")
+	 public String gifDetails(ModelMap modelMap){
+		Gif gif=new Gif("compiler-bot", LocalDate.of(2015,2,13),"Karin",true);
+		modelMap.put("gif",gif);
+		return "gif-details";
 	}
 }
