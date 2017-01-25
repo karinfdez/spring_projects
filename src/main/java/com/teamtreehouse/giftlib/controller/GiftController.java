a07e60ce2bf57cc9684e35a5bd0ea4764a49a0df
Karin Fernandez <karinfdez@yahoo.com>
2017-01-25 00:18:01 -0500

    Added list of gifs on home page

--- src/main/java/com/teamtreehouse/giftlib/controller/GiftController.java
+++ src/main/java/com/teamtreehouse/giftlib/controller/GiftController.java
@@ -1,25 +1,37 @@
 package com.teamtreehouse.giftlib.controller;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.teamtreehouse.giftlib.model.Gif;
 
 
+import com.teamtreehouse.giftlib.data.GifRepository;
+
+
 @Controller
 public class GiftController {
+	
+	@Autowired
+	private GifRepository gifRepository;
+	
 
 	@RequestMapping("/")
-	public String listGifs(){
+	public String listGifs(ModelMap modelMap) {
+		List<Gif> gifList=gifRepository.getList();
+		modelMap.put("gifs", gifList);
 		return "home";
 	}
 	
-	@RequestMapping("/gif")
-	 public String gifDetails(ModelMap modelMap){
-		Gif gif=new Gif("compiler-bot", LocalDate.of(2015,2,13),"Karin",true);
+	@RequestMapping("/gif/{name}")
+	 public String gifDetails(@PathVariable String name,ModelMap modelMap){
+		Gif gif=gifRepository.findByName(name);
 		modelMap.put("gif",gif);
 		return "gif-details";
 	}
---
+++ src/main/java/com/teamtreehouse/giftlib/data/GifRepository.java
@@ -0,0 +1,35 @@
+package com.teamtreehouse.giftlib.data;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.teamtreehouse.giftlib.model.Gif;
+
+@Component
+public class GifRepository {
+	private static final List<Gif> ALL_GIFS=Arrays.asList(
+			new Gif("android-explosion", LocalDate.of(2015,2,13), "Chris Ramacciotti", false),
+			new Gif("ben-and-mike", LocalDate.of(2015,10,30), "Ben Jakuben", true),
+			new Gif("book-dominos", LocalDate.of(2015,9,15), "Craig Dennis", false),
+			new Gif("compiler-bot", LocalDate.of(2015,2,13), "Ada Lovelace", true),
+			new Gif("cowboy-coder", LocalDate.of(2015,2,13), "Grace Hopper", false),
+			new Gif("infinite-andrew", LocalDate.of(2015,8,23), "Marissa Mayer", true)
+	);
+	
+	public Gif findByName(String name) {
+		for (Gif gif : ALL_GIFS){
+			if(gif.getName().equals(name)){
+				return gif;
+			}
+		}
+		return null;
+	}
+	
+	public List<Gif> getList() {
+		return ALL_GIFS;
+	}
+
+}
